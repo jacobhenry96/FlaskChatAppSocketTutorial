b9828c37673d1663db0881443f0bdbc3ced4e399
jacobhenry96 <jacobkhenry2@gmail.com>
2025-01-03 09:14:25 -0600

    chat funtcionality added

--- main.py
+++ main.py
@@ -55,6 +55,20 @@ def room():
         return redirect(url_for("home"))
     return render_template("room.html")
 
+@socketio.on("message")
+def message(data):
+    room = session.get("room")
+    if room not in rooms:
+        return
+    content = {
+        "name": session.get("name"),
+        "message": data["data"]
+    }
+    send(content, to=room)
+    rooms[room]["messages"].append(content)
+    print(f"{session.get("name")} said: {data['data']}")
+
+
 @socketio.on("connect")
 def connect(auth):
     room = session.get("room")
